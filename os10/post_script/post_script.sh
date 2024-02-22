e0ded9a5aaf192de3c5842982450580cb0b216cd
Plukkie <plukkie@gmail.com>
2024-02-22 19:38:25 +0000

    tftpboot initial

---
+++ os10/post_script/post_script.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+## Set SFDMODE to 1 converts switch to SFD managed, set to 0 puts in normal mode
+MGTPREFIX="10.10.10"
+MGTNET=0
+TFTP_HOST=201
+declare -i GW=$MGTNET+1
+MGTMASK="/24"
+TFTPSERVER="${MGTPREFIX}.${TFTP_HOST}"
+TFTP_PATH="tftpboot"
+CALLBACK_PATH="callback"
+STAGING_FINISHED=/ztp_finished
+OOB_INT=eth0
+USER_NAME=admin
+TMP=/tmp
+HOME=/home
+ADMIN_HOME="${HOME}/${USER_NAME}"
+PASSWORD=admin
+MGMT_IP=localhost
+APP=https://
+GREP=`type -tP grep`
+CURL=`type -tP curl`
+SED=`type -tP sed`
+ZTD_LOGFILE=post_script.log
+LOG=$TMP/$ZTD_LOGFILE
+ZTP_HOSTNAME="ZTP_NO_NAME_FOUND"
+ZTPFINISH="_ztp.finished"
+
+
+## Find DHCP client IP received
+DHCP_IP=`grep fixed-address /var/lib/dhcp/dhclient.eth0.leases | tail -1 | sed 's/.* //g' | sed 's/;//'`
+
+echo "DHCP IP Address received : $DHCP_IP" 2>&1 | tee -a $LOG && sleep 2
+echo "gateway received : $GW" 2>&1 | tee -a $LOG && sleep 2
+
+## Extract the mac address of the switch
+MAC=`ip link show eth0 | grep -o -P "(?<=ether ).*(?= brd)" | sed 's/:/_/g'`
+
+echo "MAC address switch mgt interface : $MAC" 2>&1 | tee -a $LOG && sleep 2
+
+echo "Fetching file $MAC.txt to discover my hostname..." 2>&1 | tee -a $LOG && sleep 2
+
+if hostname=`curl -s -f --interface ${OOB_INT} http://${TFTPSERVER}/${TFTP_PATH}/${CALLBACK_PATH}/${MAC}.txt`
+   then
+      echo "Succesfully found hostname : $hostname" 2>&1 | tee -a $LOG
+      SWITCHNAME=$hostname
+   else
+      SWITCHNAME=$ZTP_HOSTNAME
+      echo "Did not find hostname for mac $MAC, hostname will be : $SWITCHNAME" 2>&1 | tee -a $LOG
+fi
+
+sleep 2
+
+## Set hostname
+echo "Configuring hostname $SWITCHNAME via API..." 2>&1 | tee -a $LOG && sleep 2
+apicall=`curl -f -s -i -k -H "Accept: application/json" -H "Content-Type: application/json" -u $USER_NAME:$PASSWORD -d'{"dell-system:system":{"hostname": "'"$SWITCHNAME"'" }}' -X PATCH $APP$MGMT_IP/restconf/data/dell-system:system`
+
+## Upload ZTP staging complete file to web server
+ZTPFINISHFILE=`echo ${DHCP_IP}`${ZTPFINISH}
+echo $SWITCHNAME > $TMP/${ZTPFINISHFILE} 
+$CURL --interface $OOB_INT -T $TMP/$ZTPFINISHFILE tftp://${TFTPSERVER}${STAGING_FINISHED}/${ZTPFINISHFILE} 2>&1 | tee -a $LOG`
+echo "ZTP has finished. Uploading file '$ZTPFINISHFILE' to server 'tftp://${TFTPSERVER}${STAGING_FINISHED}/${ZTPFINISHFILE}'" 2>&1 | tee -a $LOG && sleep 2
+
+
---
+++ os10/ztd/ztd_gns3_dev_fabric.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+ZTD_SERVER_IP="10.10.10.201"
+CGI_HOSTNAME_SCRIPT=/cgi-bin/catch_hostnames.sh
+ZTD_PATH=/tftpboot
+OS_PATH=/os10
+HOSTNAMES=hostnames.dyn
+IMAGE_PATH=/osimages
+OS_IMAGE=onie-installer-x86_64
+POSTSCRIPT_PATH=/post_script
+POSTSCRIPT_FILE=post_script.sh
+CLI_CONF_FILE=cli_config
+CLI_CONF_PATH=/cli_config
+TMP=/tmp/
+HOME=/home
+ZTD_LOGFILE=ztd.log
+LOG=$TMP$ZTD_LOGFILE
+APP="http://"
+CURL=`type -tP curl`
+
+#IMG_FILE="$APP$ZTD_SERVER_IP$ZTD_PATH$IMAGE_PATH/$OS_IMAGE"
+CLI_CONFIG_FILE="$APP$ZTD_SERVER_IP$ZTD_PATH$OS_PATH$CLI_CONF_PATH/$CLI_CONF_FILE"
+POST_SCRIPT_FILE="$APP$ZTD_SERVER_IP$ZTD_PATH$OS_PATH$POSTSCRIPT_PATH/$POSTSCRIPT_FILE"
+
+## Execute server side script that will fetch hostnames and mac mapping
+#$CURL $APP$ZTD_SERVER_IP$CGI_HOSTNAME_SCRIPT
+
+#echo "Request server side hostname mapping preperation :  $APP$ZTD_SERVER_IP$CGI_HOSTNAME_SCRIPT" >> $LOG
+
+## Download the file with hostname to mac mapping
+#$CURL -o $HOME/$HOSTNAMES $APP$ZTD_SERVER_IP$ZTD_PATH/$HOSTNAMES
+
+#echo "Retreived $HOSTNAMES from server and saved to $HOME/$HOSTNAMES" >> $LOG
+
+################### DO NOT MODIFY THE LINES BELOW ##################
+sudo os10_ztd_start.sh "$IMG_FILE" "$CLI_CONFIG_FILE" "$POST_SCRIPT_FILE"
+############################# **END** #################################
